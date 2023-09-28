f18ce0c06d6f5c50eac8355dfb2f28035fa96212
Abijithgangadharan <111345193+Abijithgangadharan@users.noreply.github.com>
2023-09-28 18:41:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -147,11 +147,20 @@ resource "aws_lb_target_group" "ElasticLoadBalancingV2TargetGroup" {
     vpc_id = "vpc-09944f1f6835d0fb8"
     name = "angular-tg"
 }
-  backend "s3" {
-    bucket         	   = "angular-abi"
-    key              	   = "state/terraform.tfstate"
-    region         	   = "us-east-1"
-    encrypt        	   = true
-    dynamodb_table = "angular-db"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.18.0"
+    }
   }
+
+  backend "s3" {
+    bucket         	   = "mycomponents-tfstate"
+    key              	   = "state/terraform.tfstate"
+    region         	   = "eu-central-1"
+    encrypt        	   = true
+    dynamodb_table = "mycomponents_tf_lockid"
+  }
+}
 

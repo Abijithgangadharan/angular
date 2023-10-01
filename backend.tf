063805414527399cba742bbf20d5f1abc4cd812f
Abijithgangadharan <111345193+Abijithgangadharan@users.noreply.github.com>
2023-10-01 13:21:39 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ terraform {
     key              	   = "state/terraform.tfstate"
     region         	   = "us-east-1"
     encrypt        	   = true
-    dynamodb_table = "angular-db"
+    #dynamodb_table = "angular-db"
   }
 }
